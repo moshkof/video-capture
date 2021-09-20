548964519249d152669ba7d0f8ff92f8430ee2ba
cataclysm78 <cataclysm78@yandex.ru>
2021-09-20 15:00:49 +0300

    переименование файла main в video_capture

--- video_capture.py
+++ video_capture.py
@@ -5,15 +5,12 @@ from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 
 def run_analysis():
-    # cap = cv2.VideoCapture('https://camera.rt.ru/66ca723a-c024-4877-b279-f7db9189931d')
-    # font_size=14
-    # font_color=(0,0,0)
-    # unicode_font = ImageFont.truetype("Arial.ttf", font_size)
     unicode_text = u'перекресток маршала Говорова и Балтийская'
     cap = cv2.VideoCapture()
-    cap.open('rtsp://admin:P123456i@192.168.18.21')
-    # cap.open('rtsp://admin:P54321k@192.168.18.38')
-
+    # cap.open('rtsp://admin:P123456i@192.168.18.89:554')
+    cap.open('http://94.72.19.56/mjpg/video.mjpg')
+    # cap.open('https://youtu.be/NyygAOnP5nY')
+    print(cap.isOpened())
     if not cap.isOpened():
         print("Error opening video")
 
@@ -21,11 +18,9 @@ def run_analysis():
         status, frame = cap.read()
         if status:
             cv2.imshow(unicode_text, frame)
-            # draw  =  ImageDraw.Draw ( cap )
-            # draw.text ( (10,10), unicode_text, font=unicode_font, fill=font_color )
-            # do_stuff_with_frame(frame)
         key = cv2.waitKey(25)
         if key == ord('q'):
             break
+
 
 run_analysis()
